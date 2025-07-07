443ad102441ff862ff6059d5fdcf91a172754272
OllieOlzu <65995434+OllieOlzu@users.noreply.github.com>
2025-07-07 17:16:00 +1200

    Update MovingTreeBranches.c

--- MovingTreeBranches.c
+++ MovingTreeBranches.c
@@ -1,4 +1,6 @@
-//Use the A key to move the branches down the tree. This is my my test for a bigger project I'm working on for a tree chopping game, that is simalar to Bob and chainsaw. You can run these on Linux at https://github.com/OllieOlzu/Easy-32-bit-C-kernel-to-ISO.
+//Use the A key to move the branches down the tree. 
+//This is my my test for a bigger project I'm working on for a tree chopping game, that is simalar to Bob and chainsaw. 
+//You can run these on Linux at https://github.com/OllieOlzu/Easy-32-bit-C-kernel-to-ISO.
 
 #include <stdint.h>
 
